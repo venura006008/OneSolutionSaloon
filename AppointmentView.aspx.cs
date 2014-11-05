6433d1ca173b0dd174e7427c5b837502f6e08afc
venura006008 <venura006008@gmail.com>
2014-11-06 01:35:53 +0530

    Adding
    Addmin

---
+++ AppointmentView.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OneSolutionSaloon
+{
+    public partial class AppointmentView : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
